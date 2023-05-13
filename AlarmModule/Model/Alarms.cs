42cb6afc6dee14cae1278a9862edb089c533f1cd
Amila Ruwan Guruge <amilaruwan9349@gmail.com>
2023-05-13 07:51:27 +0200

    Add project files.

---
+++ AlarmModule/Model/Alarms.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace AlarmModule.Models
+{
+    public class Alarms
+    {
+        public int AlarmId { get; set; }
+        public DateTime ActivationTime { get; set; }
+        public string AlarmType { get; set; }
+        public string Information { get; set; }
+        public DateTime? AcknowledgeTime { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Alarms> GetAlarmData()
+        {
+            string connectionString = "Data Source=DESKTOP-D4R0VU6\\WINCC;Initial Catalog=SCADA_SYSTEM;Integrated Security=True;TrustServerCertificate=True";
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            string sqlQuery = "SELECT * FROM GetAlarmInformation ORDER BY ActivationTime DESC;";
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            List<Alarms> alarmList = new List<Alarms>();
+
+            while (dr.Read())
+            {
+                Alarms alarms = new Alarms();
+
+                alarms.AlarmId = Convert.ToInt32(dr["AlarmId"]);
+                alarms.ActivationTime = Convert.ToDateTime(dr["ActivationTime"]);
+                alarms.AlarmType = dr["AlarmType"].ToString();
+                alarms.Information = dr["Information"].ToString();
+                if (dr["AcknowledgeTime"] == DBNull.Value)
+                {
+                    alarms.AcknowledgeTime = null; ;
+                }
+                else
+                {
+                    alarms.AcknowledgeTime = Convert.ToDateTime(dr["AcknowledgeTime"]);
+                }
+                alarms.Name = dr["Name"].ToString();
+                alarms.Description = dr["Description"].ToString();
+
+                alarmList.Add(alarms);
+            }
+            con.Close();
+            return alarmList;
+        }
+    }
+}
---
+++ AlarmModule/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using AlarmModule.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AlarmModule.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Alarms> alarmList = new List<Alarms>();
+        public void OnGet()
+        {
+            Alarms alarms = new Alarms();
+            alarmList = alarms.GetAlarmData();
+        }
+
+        public IActionResult OnGetAlarms()
+        {
+            Alarms alarms = new Alarms();
+            var alarmData = alarms.GetAlarmData();
+            return new JsonResult(alarmData);
+        }
+    }
+}
